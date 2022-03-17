84f5b1fc81d52214803b0d992d2b375be6b42c0e
alperen inci <alperen25inci@hotmail.com>
2022-03-17 17:00:53 +0300

    Navbar Kısmı Eklendi.

--- vip-front/src/App.js
+++ vip-front/src/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 import Yolcu from './pages/Yolcu';
 import YolcuEkle from './pages/YolcuEkle';
-import YolcuDuzenle from './pages/YolcuDuzenle'
+import YolcuDuzenle from './pages/YolcuDuzenle';
+import Araclar from './pages/Araclar';
+import Suruculer from './pages/Suruculer';
+import Transferler from './pages/Transferler';
+import Navbar from './pages/Navbar';
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path ="/" component={Yolcu}/>
-        <Route path ='/Yolcu_Ekle' component={YolcuEkle}/>
-        <Route path='/Yolcu_duzenle/:id'component={YolcuDuzenle}/>
-      </Switch>
-    </Router>
+      <Router>
+          <Navbar/>
+          <Switch>
+              <Route path ="/" exact component={Yolcu}/>
+              <Route path ='/Yolcu_Ekle' component={YolcuEkle}/>
+              <Route path='/Yolcu_duzenle/:id'component={YolcuDuzenle}/>
+              <Route path='/araclar'component={Araclar}/>
+              <Route path='/suruculer'component={Suruculer}/>
+              <Route path='/transferler'component={Transferler}/>
+          </Switch>
+     </Router>
   );
 }
 
---
+++ vip-front/src/pages/Araclar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '90vh'
+            }}
+        >
+            <h1>Mercedes Vito</h1>
+        </div>
+    );
+};
+
+export default App;
